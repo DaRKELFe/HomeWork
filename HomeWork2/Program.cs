4d742cc8e51386e7eac612f63c13fb98d88037cb
DaRKELFe <107640944+DaRKELFe@users.noreply.github.com>
2022-07-04 20:09:36 +1200

    hello world2:)

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*void CutNumber()
+{
+
+    string num = Console.ReadLine();
+
+    Console.WriteLine("{0}->{1}",num, num[1]);
+    
+
+
+}
+
+CutNumber();
+*/
+/*
+void CutNumber()
+{
+
+    int num = Convert.ToInt32(Console.ReadLine());
+     if(num/100==0)
+ m\
+ ]l
+ \o
+ \
+ \lo
+    Console.WriteLine("Net chisla");
+    string reshenie = Convert.ToString(num);
+    Console.WriteLine("{0}->{1}",reshenie, reshenie[2]);
+}
+
+CutNumber();
+*/
+
+void CutNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    if(num>5)
+    Console.WriteLine("Yes");
+    else 
+    Console.WriteLine("No");
+}
+
+CutNumber();
