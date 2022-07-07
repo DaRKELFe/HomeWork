010c09d5f22aa410b2164ea10ba75935da6258c4
DaRKELFe <107640944+DaRKELFe@users.noreply.github.com>
2022-07-07 21:29:37 +1200

    hello world 3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,51 @@
+﻿/*void Number()
+{
+    Console.WriteLine("press push a button");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int oldValue = number;
+      int newValue = 0;
+     while (number > 0)
+     {
+        int dig = number % 10;
+         newValue = newValue * 10 + dig;
+         number = number / 10;
+    }
+    if (newValue == oldValue)
+      Console.WriteLine("Palindrom");
+    else
+    Console.WriteLine("Not Palindrom");
+     Console.Write("press push alt + F4");
+     Console.ReadKey();
+}
+
+Number();
+
+*/
+
+Console.Write("Press start");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int length = cube.Length;
+  for (int counter = 0; counter <  length; counter++)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    
+  }
+}
+
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  
+  for(int index = 0;index < count;index++)
+  {
+    Console.Write(coll[index]+ " ");
+    
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
